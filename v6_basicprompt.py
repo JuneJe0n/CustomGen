ca30c9b5e7223d29215d4f893d6ec465378f846c
JuneJe0n <jyunejeon@gmail.com>
2026-05-10 02:34:23 +0900

    add: v6_basicprompt.py, cache offloading

--- v6_basicprompt.py
+++ v6_basicprompt.py
@@ -39,6 +39,8 @@ def main(face_img_path: str, pose_img_path: str, style_img_path: str, output_pat
     from utils import BasicPromptGenerator
     generator = BasicPromptGenerator()
     prompt = generator.generate_combined_prompt(face_img_path, pose_img_path)
+    del generator
+    torch.cuda.empty_cache()
     W, H = pose_im.size
 
 
@@ -62,6 +64,7 @@ def main(face_img_path: str, pose_img_path: str, style_img_path: str, output_pat
     fx1, fy1, fx2, fy2 = map(int, f_info['bbox'])
     fw, fh = fx2 - fx1, fy2 - fy1
     face_crop_pil = face_im.crop((fx1, fy1, fx2, fy2))
+    
 
     # Face bbox from pose img
     pose_cv = cv2.cvtColor(np.array(pose_im), cv2.COLOR_RGB2BGR)
@@ -73,6 +76,8 @@ def main(face_img_path: str, pose_img_path: str, style_img_path: str, output_pat
     x1, y1 = max(0, x1), max(0, y1)
     x2, y2 = min(W, x2), min(H, y2)
     pw, ph = x2 - x1, y2 - y1
+    del face_det
+    torch.cuda.empty_cache()
 
 
 
@@ -87,6 +92,9 @@ def main(face_img_path: str, pose_img_path: str, style_img_path: str, output_pat
     face_hed_crop_pil = hed(face_crop_pil, safe=False, scribble=False)
     face_hed_resized  = face_hed_crop_pil.resize((new_w, new_h), Image.LANCZOS)
     face_hed_np       = np.array(face_hed_resized).astype(np.float32)
+    del hed
+    torch.cuda.empty_cache()
+
 
     # Resize face crop
     face_crop_pil_resized = face_crop_pil.resize((new_w, new_h), Image.LANCZOS)
@@ -153,6 +161,9 @@ def main(face_img_path: str, pose_img_path: str, style_img_path: str, output_pat
     pose_openpose_pil = openpose(integrated_canvas_pil, hand_and_face=True).resize((W, H), Image.LANCZOS)
     # pose_openpose_pil.save(output_dir/ "7_pose_kps.png")
     pose_openpose_np  = np.array(pose_openpose_pil).astype(np.float32) # openpose skeleton img
+    del openpose
+    torch.cuda.empty_cache()
+
 
     # Insert face HED on pose img size empty canvas
     face_hed_canvas_np = np.zeros_like(pose_openpose_np, dtype=np.float32) # empty canvas of size pose
